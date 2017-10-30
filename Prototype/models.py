f477096f117b67c978c2a3ca3f7a46f78d78d8b3
SimonMarkWhittle <smw2ed@virginia.edu>
2017-10-29 20:01:17 -0400

    removed dummy code

--- Prototype/models.py
+++ Prototype/models.py
@@ -1,29 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-
-"""
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from .forms import ModelFormWithFileField
-
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = ModelFormWithFileField(request.POST, request.FILES)
-        if form.is_valid():
-            # file is saved
-            form.save()
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = ModelFormWithFileField()
-    return render(request, 'upload.html', {'form': form})
-
-
-def handle_uploaded_file(f):
-    with open('upload.txt', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-"""
--- Prototype/views.py
+++ Prototype/views.py
@@ -13,27 +13,3 @@ def simple_upload(request):
             'uploaded_file_url': uploaded_file_url
         })
     return render(request, 'simple_upload.html')
-
-"""
-
-from django.views.generic.edit import FormView
-from .forms import FileFieldForm
-from .models import handle_uploaded_file
-
-
-class FileFieldView(FormView):
-    form_class = FileFieldForm
-    template_name = 'upload.html'  # Replace with your template.
-    success_url = '...'  # Replace with your URL or reverse().
-
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('file_field')
-        if form.is_valid():
-            for f in files:
-                handle_uploaded_file(f)
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-"""
